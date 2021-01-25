1c42303d2854f679979af33f0defbca8df2d7d21
ttanimarefin <ttanimarefin@gmail.com>
2021-01-25 21:29:38 +0600

    Work on Templating

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
