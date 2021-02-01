ee2467e4f7fdd2618fa6617724565e60104b2946
ttanimarefin <ttanimarefin@gmail.com>
2021-02-01 09:42:30 +0600

    Hooking up  database to front end

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///posts.db'
 db=SQLAlchemy(app)
 
+class BlogPost (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title=db.Column(db.String(100),nullable=False)
+    content=db.Column(db.Text,nullable=False)
+    author=db.Column(db.String(20),nullable=False, default='N/A')
+    date_posted=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post' + str (self.id)
+        
 
 all_posts = [
     {
@@ -14,7 +25,7 @@ all_posts = [
     },
     {
         'title':'Post 2',
-        'content' : 'This is the content of  post2',
+        'content' : 'This is the content of  post2'
         
     }
 ]
@@ -27,10 +38,24 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/post')
-@app.route('/')
+
+
+@app.route('/posts', methods=['GET','POST'])
 def posts():
-    return render_template('posts.html',posts=all_posts)
+    if request.method=='POST':
+        post_title= request.form['title']
+        post_content=request.form['content']
+        new_post= BlogPost(title=post_title,content=post_content,author='Tanim')
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+
+    else:
+        all_posts=BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts= all_posts)
+    
+    
+
 
 #replay  name
 @app.route('/tune/<string:name>')
@@ -42,8 +67,8 @@ def tune(name):
 def user(name,id):
     return "hello," +name+ ", your id:" + str(id)
 
-
-app.run(debug=True,port=8080)
+if __name__=="__main__":
+    app.run(debug=True)
 
 
 
